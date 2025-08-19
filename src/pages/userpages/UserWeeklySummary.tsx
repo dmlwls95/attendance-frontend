b8658ddec4707bc2ac2b73bd627a0a7c1a377f9c
Joy <jtograce@gmail.com>
2025-08-19 21:21:47 +0900

    API연동중 주석 수정

--- src/pages/userpages/UserWeeklySummary.tsx
+++ src/pages/userpages/UserWeeklySummary.tsx
@@ -3,7 +3,7 @@ import WeeklyTable from "../../components/WeeklyTable";
 import WeeklyLateStatus from "../../components/WeeklyLateStatus";
 
 export default function UserWeeklySummary() {
-  // 임시로 startDate, endDate를 지정 (추후 API 데이터로 교체 가능)
+  // 임시로 startDate, endDate를 지정 (API연동하려다 재차 실패해서 오시면 여쭤보겠습니다!)
   const startDate = "2025-08-01";
   const endDate = "2025-08-07";
 
