4728e5f5ef2ef1427c36113c1cb5c0702ad338e6
Joy <jtograce@gmail.com>
2025-08-19 21:18:45 +0900

    UserWeeklySummary와 연동

--- src/pages/userpages/UserWeeklySummary.tsx
+++ src/pages/userpages/UserWeeklySummary.tsx
@@ -1,10 +1,17 @@
-export default function UserWeeklySummary()
-{
-   
-    return (
-        <div>
+import WeeklyAnalysis from "../../components/WeeklyAnalysis";
+import WeeklyTable from "../../components/WeeklyTable";
+import WeeklyLateStatus from "../../components/WeeklyLateStatus";
 
-        </div>
-       
-    );
+export default function UserWeeklySummary() {
+  // 임시로 startDate, endDate를 지정 (추후 API 데이터로 교체 가능)
+  const startDate = "2025-08-01";
+  const endDate = "2025-08-07";
+
+  return (
+    <div className="fullscreen">
+      <WeeklyAnalysis startDate={startDate} endDate={endDate} />
+      <WeeklyTable />
+      <WeeklyLateStatus />
+    </div>
+  );
 }
