1671e0e65225818fd140f9a4484519b038288d4e
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2025-08-25 02:10:58 +0900

    NavMemberCard 약간의 코드 정리
    1. 배경 추가
    2. card 컴포넌트 사용시 사진, 글 로그아웃 버튼의 높이가 제각각이라 여백이 너무 커져서 직접 스타일링 하는 방식으로 변경
    3. 약간의 줄 맞춤

--- src/components/NavMemberCard.tsx
+++ src/components/NavMemberCard.tsx
@@ -4,17 +4,16 @@ import RoleBadge from "./RoleBadge";
 import { useNavigate } from "react-router-dom";
 
 type NavDataResponse = {
-	name: string,
-	empno : string,
-	rank : string,
-	profileUrl : string,
-    role : string
+    name: string,
+    empno: string,
+    rank: string,
+    profileUrl: string,
+    role: string
 }
-export default function NavMemberCard() 
-{
+export default function NavMemberCard() {
     const navigate = useNavigate();
     const [navdata, setNavdata] = useState<NavDataResponse | null>(null);
-    
+
     useEffect(() => {
         (async () => {
             try {
@@ -26,62 +25,53 @@ export default function NavMemberCard()
             }
         })();
     }, []);
-    
-    const onClickLogout = () =>{
+
+    const onClickLogout = () => {
         localStorage.removeItem("token");
         navigate("/");
     }
 
     return (
         <div>
-            <div className="card card-xs bg-base-20 ">
-            <div className="card-body">
-                <div className="flex">
-
-                    <div className="avatar w-1/5">
-                        <div className="w-12 rounded-full">
-                            <img src={navdata?.profileUrl} />
-                        </div>
+            <div className="flex items-center bg-gray-500 rounded-xl px-2 py-1 h-14 gap-3">
+                <div className="avatar">
+                    <div className="w-10 h-10 rounded-full ">
+                        <img src={navdata?.profileUrl} />
                     </div>
-
-                    <div className="w-3/5">
-                        
-                        <h3> {navdata?.rank} + No. {navdata?.empno}</h3>
-                        <h3>{navdata?.name} 様 {RoleBadge(navdata?.role)}</h3>
-                    </div>
-
-
-                    <div className="card-actions justify-end w-1/5">
-                    <button className="btn btn-secondary" onClick={onClickLogout}>logout</button>
-                    </div>
-
                 </div>
-                
-                
-            </div>
+
+                <div className="flex flex-col text-xs leading-tight">
+
+                    <span>{navdata?.rank} + No. {navdata?.empno}</span>
+                    <span>{navdata?.name} 様 {RoleBadge(navdata?.role)}</span>
+                </div>
+
+
+                <div className="ml-auto">
+                    <button className="btn btn-secondary btn-base h-10 min-h-0 px-2" onClick={onClickLogout}>logout</button>
+                </div>
             </div>
         </div>
     )
-    
 
-    async function getNavBarData() : Promise<NavDataResponse> {
+
+    async function getNavBarData(): Promise<NavDataResponse> {
         const token = localStorage.getItem("token");
-                const response = await fetch(
-                    `${APIConfig}/auth/navdata` , {
-                        method: "GET",
-                        headers : {
-                            "Contetn-Type" : "application/json",
-                            "Authorization" : `Bearer ${token}`
-                        },
-                        credentials: "include"
-                    }
-                );
-        
-                if(!response.ok)
-                {
-                    throw new Error("조회 실패");
-                }
-                return await response.json();
+        const response = await fetch(
+            `${APIConfig}/auth/navdata`, {
+            method: "GET",
+            headers: {
+                "Contetn-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            credentials: "include"
+        }
+        );
+
+        if (!response.ok) {
+            throw new Error("조회 실패");
+        }
+        return await response.json();
     }
 }
 
