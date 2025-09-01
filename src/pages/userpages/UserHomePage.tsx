7e5170ce88de8572012efd92b6ad1f24eb4c4b21
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2025-09-01 19:08:26 +0900

    UserHomePage 수정
    UI 수정
    
    출근, 퇴근, 외출, 복귀 버튼 관련
    - onclick 이벤트 수정
    - disable 수정

--- src/pages/userpages/UserHomePage.tsx
+++ src/pages/userpages/UserHomePage.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-
+import dayjs from "dayjs"
 import { IoSunny } from "react-icons/io5";
 import { FaMoon } from "react-icons/fa";
 import { type BoardResponse, getRecentAttendanceRecord, getRecommendedBoardList, type AttendanceEventResponse, postCheckIn, hasCheckedInToday, postCheckOut, postOutingStart, hasBreakOut, postOutingEnd } from "../../services/UserHomepageService";
@@ -7,25 +7,25 @@ import { useNavigate } from "react-router-dom";
 import { useAttendanceStomp } from "../../hooks/useAttendanceStomp";
 
 interface AttendanceSignal {
-    type : | 'CLOCK_IN'
-  | 'BREAK_OUT'
-  | 'BREAK_IN'
-  | 'CLOCK_OUT'
-  | 'CORRECTION';
-    at : Date;
+    type: | 'CLOCK_IN'
+    | 'BREAK_OUT'
+    | 'BREAK_IN'
+    | 'CLOCK_OUT'
+    | 'CORRECTION';
+    at: Date;
 
 }
-export default function UserHomePage()
-{
+export default function UserHomePage() {
     const navigate = useNavigate();
     const [nowClock, SetNowClock] = useState<Date>(new Date());
     const [userAttendanceLog, SetUserAttendanceLog] = useState<AttendanceEventResponse[]>();
     const [recentTopBoard, SetRecentTopBoard] = useState<BoardResponse[]>();
 
     const [hasAlreadyChecked, SetHasAlreadyChecked] = useState<boolean>();
-    const [loading, setLoading] = useState(false);
+    const [clockIn_out, SetClockIn_Out] = useState<boolean>(false);
 
     const [hasOutingStarted, SetHasOutingStarted] = useState<boolean>();
+    const [outStart_end, SetOutStart_End] = useState<boolean>(false);
 
     //시계 설정
     useEffect(() => {
@@ -40,18 +40,18 @@ export default function UserHomePage()
     useEffect(() => {
         (async () => {
             const res = await getRecentAttendanceRecord();
-            if(typeof(res) === "string"){
+            if (typeof (res) === "string") {
                 return;
             }
             SetUserAttendanceLog(res);
         })();
-    },[])
+    }, [])
 
     //보드 리스트 가져오기
     useEffect(() => {
         (async () => {
             const res = await getRecommendedBoardList();
-            if(typeof(res)==="string"){
+            if (typeof (res) === "string") {
                 return;
             }
 
@@ -60,7 +60,7 @@ export default function UserHomePage()
     }, [])
 
     //인기글 이동 함수
-    const onClickBoardTitle = (id : number, type: string) =>{
+    const onClickBoardTitle = (id: number, type: string) => {
         navigate(`/user/userboard/detail/${id}/${type}`);
     }
 
@@ -69,166 +69,181 @@ export default function UserHomePage()
         (async () => {
             const res = await hasCheckedInToday();
             SetHasAlreadyChecked(res);
+            console.log("clokcIn : ", res)
         })();
-    },[])
+    }, [])
 
     useEffect(() => {
         (async () => {
             const res = await hasBreakOut();
             SetHasOutingStarted(res);
+            console.log("outing : ", res)
         })();
-    },[])
+    }, [])
 
     // stomp 수신시에 서버에 다시 물어보고 결과로만 업데이트
-    const onLiveEvent = useCallback((msg : AttendanceSignal) => {
-        if(msg.type == 'CLOCK_IN')
-        {
+    const onLiveEvent = useCallback((msg: AttendanceSignal) => {
+        if (msg.type == 'CLOCK_IN') {
             hasCheckedInToday()
-            .then(SetHasAlreadyChecked)
-            .catch(console.error)
-        }else if(msg.type =='BREAK_OUT')
-        {
+                .then(SetHasAlreadyChecked)
+                .catch(console.error)
+        } else if (msg.type == 'BREAK_OUT') {
             hasBreakOut()
-            .then(SetHasOutingStarted)
-            .catch(console.error)
+                .then(SetHasOutingStarted)
+                .catch(console.error)
         }
     }, []);
     useAttendanceStomp(onLiveEvent);
 
-    
-
-    
-
     //출근
     const onClickClockIn = () => {
         (async () => {
-            try{
-                setLoading(true);
+            try {
                 await postCheckIn();
-            }finally{
+            } finally {
                 const checked = await hasCheckedInToday();
                 SetHasAlreadyChecked(checked);
-                setLoading(false);
+                SetClockIn_Out(prev => !prev);
             }
         })();
     }
     //퇴근
     const onClickClockOut = () => {
         (async () => {
-            await postCheckOut();
-
+            try {
+                await postCheckOut();
+            } finally {
+                SetClockIn_Out(prev => !prev);
+            }
         })();
     }
 
-    //조퇴
+    // 외출
     const onClickOuting = () => {
         (async () => {
-            await postOutingStart();
+            try {
+                await postOutingStart();
+            } finally {
+                const checked = await hasBreakOut();
+                SetHasOutingStarted(checked);
+                SetOutStart_End(prev => !prev);
+            }
         })();
     }
 
     //복귀
     const onClickOutingEnd = () => {
         (async () => {
-            await postOutingEnd();
+            try {
+                await postOutingEnd();
+            } finally {
+                SetOutStart_End(prev => !prev);
+            }
         })();
     }
 
-    
-   
+    useEffect(() => {
+        console.log("clockIn_Out : ", clockIn_out)
+        console.log("outStart_end : ", outStart_end)
+
+    }, [clockIn_out, outStart_end])
+
     return (
         <div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-                <div className="col-span-1 md:col-span-1">
-                    <div className="card  bg-base-100 shadow-sm card-border">
-                        <div className="card-body">
-                            
-                        </div>
-                    </div>
-
-                </div>
-                <div className="col-span-1 md:col-span-2">
-                    <div className="card  bg-base-100 shadow-sm card-border">
-                        <div className="grid grid-rows-2 justify-center gap-4">
-                            <div className="grid grid-cols-3">
-                                <div className="col-span-2 gap-3">
-                                    <button className="btn btn-accent h-32 w-40 font-bold text-4xl" onClick={onClickClockIn} disabled={loading || hasAlreadyChecked === true} title={hasAlreadyChecked? "이미 출근 했습니다":undefined} ><IoSunny />출근</button>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <button className="btn btn-error h-32 w-40 font-bold text-4xl" onClick={onClickClockOut}><FaMoon />퇴근</button>
-                                </div>
-                                <div className="col-span-1">
-
-                                </div>
-                            </div>
-                            
-                            <div className="grid grid-cols-3 justify-center items-center text-center">
-                                <div className="card bg-gray-200 text-black w-36 h-16 justify-center ">
-                                    <div className="items-center text-center justify-center">
-                                        <p className="justify-center items-center text-center font-bold">{nowClock.toLocaleTimeString('ko-KR') }</p>
-                                        
-                                    </div>
-                                </div>
-
-                                <button className="btn btn-info h-32 w-40 font-bold text-4xl" onClick={onClickOuting} disabled={loading || hasOutingStarted === true}>외출</button>
-                                <button className="btn btn-info h-32 w-40 font-bold text-4xl" onClick={onClickOutingEnd} disabled={loading || hasOutingStarted === false}>복귀</button>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-
-            </div>
             <br></br>
+            <hr className="border-2 border-withe" />
+            <br></br>
+   
+            <div className="flex w-full h-full mb-4 gap-3">
+                <div className="bg-gray-500 w-1/2 flex justify-center items-center rounded-xl">
+                    <div>
+                        <p className="font-bold text-7xl">{nowClock.toLocaleTimeString('ko-KR')}</p>
+                    </div>
+                </div>
+
+                <div className="w-1/2 grid grid-cols-2 grid-rows-2 gap-2">
+                    <button
+                        className="btn btn-accent h-40 w-full font-bold text-4xl rounded-xl"
+                        onClick={hasAlreadyChecked === false ? onClickClockIn : undefined}
+                        disabled={hasAlreadyChecked === true}
+                        title={hasAlreadyChecked ? "이미 출근 했습니다" : undefined}
+                    >
+                        <IoSunny />출근
+                    </button>
+
+                    <button
+                        className="btn btn-error h-40 w-full font-bold text-4xl rounded-xl"
+                        onClick={clockIn_out === true ? onClickClockOut : undefined}
+                        disabled={clockIn_out === false}
+                    >
+                        <FaMoon />퇴근
+                    </button>
+
+                    <button
+                        className="btn btn-info h-40 w-full font-bold text-4xl rounded-xl"
+                        onClick={hasAlreadyChecked === true ? onClickOuting : undefined}
+                        disabled={(hasOutingStarted === false && hasAlreadyChecked === false) ||
+                            ((hasOutingStarted === true && hasAlreadyChecked === true))
+                        }
+                    >
+                        외출
+                    </button>
+
+                    <button
+                        className="btn btn-info h-40 w-full font-bold text-4xl rounded-xl"
+                        onClick={outStart_end === true ? onClickOutingEnd : undefined}
+                        disabled={outStart_end === false}
+                    >
+                        복귀
+                    </button>
+
+                </div>
+            </div>
+
             <div className="grid grid-cols-2 gap-3">
                 <div className="card bg-base-100 shadow-sm card-border">
-                    <div className="card-body whitespace-nowrap">
+                    <div className="card-body whitespace-nowrap p-1">
                         {/* 헤더 */}
-                        <li className="list-row font-semibold text-sm border-b py-2">
-                            <div className="grid grid-cols-9 gap-20 text-center">
+                        <ol className="list-row font-semibold text-sm border-b py-2">
+                            <div className="grid grid-cols-9 gap-60 text-center">
                                 <div>날짜</div>
                                 <div>상태</div>
                             </div>
-                        </li>
+                        </ol>
                         {
                             userAttendanceLog?.map((record, idx) => (
-                                <li className="list-row text-sm border-b py-2" key={idx}>
-                                    <div className="grid grid-cols-9 gap-20 text-center">
-                                        <div>{record.createdAt}</div>
+                                <ol className="list-row text-sm border-b py-2" key={idx}>
+                                    <div className="grid grid-cols-9 gap-60 text-center">
+                                        <div>{dayjs(record.occurredAt).format("YYYY-MM-DD HH:mm:ss")}</div>
                                         <div>{record.eventType}</div>
                                     </div>
-                                </li>
+                                </ol>
                             ))
                         }
                     </div>
                 </div>
-                <div className="card bg-base-100 shadow-sm card-border">
-                    <div className="card-body whitespace-nowrap">
-                        <p className="text-sm font-bold">인기글</p>
-
+                <div className="card bg-base-100 shadow-sm card-border border-gray-500">
+                    <div className="card-body whitespace-nowrap p-1">
                         {/* 헤더 */}
-                        <li className="list-row font-semibold text-sm border-b py-2">
-                            <div className="grid grid-cols-3 gap-5 text-center">
-                                <div></div>
+                        <ol className="list-row font-semibold text-sm border-b py-2">
+                            <div className="grid grid-cols-3">
+                                <div>인기글</div>
                                 <div>제목</div>
                                 <div>등록일자</div>
                             </div>
-                        </li>
+                        </ol>
                         {
                             recentTopBoard?.map((record, idx) => (
-                                <li className="list-row text-sm border-b py-2" key={idx}>
-                                    <div className="grid grid-cols-3 gap-5 text-center">
+                                <ol className="list-row text-sm border-b py-2" key={idx}>
+                                    <div className="grid grid-cols-3">
                                         <div>{record.id}</div>
-                                        <button className="btn btn-link" onClick={() => onClickBoardTitle(record.id, record.boardType)}>{record.title}</button>
+                                        <div>{record.title}</div>
+                                        {/*<button className="btn btn-link w-1/3 h-1 p-1" onClick={() => onClickBoardTitle(record.id, record.boardType)}>{record.title}</button>*/}
                                         <div>{new Date(record.writeDate).toLocaleDateString("ko-KR")}</div>
                                     </div>
-                                </li>
+                                </ol>
                             ))
                         }
-
-
-                        
-                        
                     </div>
                 </div>
 
@@ -236,7 +251,7 @@ export default function UserHomePage()
 
 
         </div>
-       
+
     );
 
 }
